7ae773e368a91871c9e4f863a881f8524ed43289
hyojun6 <yhyojun66@gmail.com>
2024-10-07 23:35:37 +0900

    nodejs

---
+++ nodejs/mysql.js
@@ -0,0 +1,18 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'gywns',
+  password : 'q1w2e3',
+  database : 'nodejs'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT * from topic', function (error, results, fields) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+});
+ 
+connection.end();
